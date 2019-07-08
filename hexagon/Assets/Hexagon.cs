16b963014eec9724bc725e787bc16b1ee1df3476
MrKasap <MrKasap@users.noreply.github.com>
2019-07-08 17:38:54 +0300

    Add Hexagon Game

---
+++ hexagon/Assets/Hexagon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float shrinkSpeed = 3f;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.rotation = Random.Range(0f,360f);
+        transform.localScale = Vector3.one * 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale -= shrinkSpeed * Time.deltaTime * Vector3.one;
+
+        if(transform.localScale.x < .5f){
+            Destroy(gameObject);
+            score ++;
+        }
+    }
+}
---
+++ hexagon/Assets/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public float speed = 600f;
+    float movement = 0f;
+    public int playing=1;
+    public Text Text;
+    public int score;
+    // Update is called once per frame
+    void Start(){
+        score = (int) Time.time;
+    }
+
+    void Update()
+    {   if(playing==1){
+        movement = Input.GetAxisRaw("Horizontal");
+        }
+
+    }
+    private void FixedUpdate(){
+        if(playing==1){
+        transform.RotateAround(Vector3.zero,Vector3.forward,movement*Time.fixedDeltaTime*-speed);
+        }
+        else{
+            if(Input.GetMouseButtonDown(0)){
+                playing = 1;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(playing==1){
+            playing = 0;
+            score = (int)Time.time - score;
+            Text.gameObject.SetActive(true);
+            score = score-1;
+            Text.text="Your got " + score +" points" + "\nClick to replay" ;
+        }
+    }
+
+}
